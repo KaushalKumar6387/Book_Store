1d7a4379eed9a030e41272b3dfce3265327f6932
KaushalKumar6387 <kingggg6787@gmail.com>
2025-10-10 12:41:36 +0530

    navbar update css

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,39 +1,95 @@
-import { useState } from 'react'
-import logo from '../assets/book.png'
-function Navbar(){
-    return (
-        <>
-        <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+import { useState } from "react";
+import logo from "../assets/book.png";
+function Navbar() {
+  const navItem = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>Course</a>
+      </li>
+      <li>
+        <a>About Us</a>
+      </li>
+      <li>
+        <a>Contact Us</a>
+      </li>
+    </>
+  );
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="navbar bg-base-100 ">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  {" "}
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />{" "}
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              >
+                {navItem}
+              </ul>
+            </div>
+
+            <a className="btn btn-ghost text-xl">
+              <img src={logo} alt="" style={{ height: "30px" }} />
+              Bookverse
+            </a>
+          </div>
+          <div className="navbar-end">
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1">{navItem}</ul>
+            </div>
+           <div className="hidden md:block mx-2">
+             <label className="input">
+              <svg
+                className="h-[1em] opacity-50"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+              >
+                <g
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  strokeWidth="2.5"
+                  fill="none"
+                  stroke="currentColor"
+                >
+                  <circle cx="11" cy="11" r="8"></circle>
+                  <path d="m21 21-4.3-4.3"></path>
+                </g>
+              </svg>
+              <input type="search" className="grow" placeholder="Search" />
+            </label>
+           </div>
+            <div>
+              <a className="btn">Login</a>
+            </div>
+          </div>
+        </div>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-        <li><a>Home</a></li>
-      <li><a>Course</a></li>    
-      <li><a>About Us</a></li>
-      <li><a>Contact Us</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl"><img src={logo} alt="" style={{height:"40px"}} />Bookverse</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Home</a></li>
-      <li><a>Course</a></li>    
-      <li><a>About Us</a></li>
-      <li><a>Contact Us</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div>
-        </>
-    )
+    </>
+  );
 }
 
 export default Navbar;
